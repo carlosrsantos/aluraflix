700edec906774d26e5ac432c3ef83a41cf88f129
Carlos Rodrigues <crodrigsantos@gmail.com>
2022-11-14 13:05:17 -0300

    Updating solution - added tests

---
+++ Aluraflix.Tests/Models/CategoryTests.cs
@@ -0,0 +1,37 @@
+﻿
+
+using Aluraflix.Models;
+using Xunit.Abstractions;
+
+namespace Aluraflix.Tests.Models
+{
+    public class CategoryTests : IDisposable
+    {
+        public Category category { get; set; }
+
+        public ITestOutputHelper testOutputHelper;
+
+        public CategoryTests(ITestOutputHelper _testOutputHelper)
+        {
+            testOutputHelper = _testOutputHelper;
+        }
+
+
+        public void Dispose()
+        {
+            testOutputHelper.WriteLine("Dispose Invocado");
+        }
+
+        [Fact]
+
+        public void Test_Category_Color_Is_Invalid()
+        {
+            var color = "black";
+
+            Assert.Throws<System.ComponentModel.DataAnnotations.ValidationException>
+            (
+                () => new Category().Color = color
+            );
+        }
+    }
+}
