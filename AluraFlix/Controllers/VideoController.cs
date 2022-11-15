f26dff9725fa0290b684d00380e4b8b98a7e9aa6
Carlos Rodrigues <crodrigsantos@gmail.com>
2022-11-15 20:23:25 -0300

    Creating pagination of objects

--- AluraFlix/Controllers/VideoController.cs
+++ AluraFlix/Controllers/VideoController.cs
@@ -11,161 +11,181 @@ namespace Aluraflix.Controllers;
 [ApiController]
 public class VideoController : ControllerBase
 {
-  private AluraflixContext _context;
+    private AluraflixContext _context;
 
-  public VideoController(AluraflixContext context)
-  {
-    _context = context;
-  }
-
-  [Route("/api/v1/videos")]
-  [HttpGet]
-  public async Task<IActionResult> GetAsync()
-  {
-    var videos = await _context.Videos.ToListAsync();
-    return Ok(new ResultViewModel<List<Video>>(videos));
-  }
-
-  [Route("/api/v1/videos/{id}")]
-  [HttpGet]
-  public async Task<IActionResult> GetByIdAsync(
-     [FromRoute] int id)
-  {
-    var video = await _context.Videos.FirstOrDefaultAsync(x => x.Id == id);
-    if (video != null)
-      return Ok(new ResultViewModel<Video>(video));
-    else
-      return NotFound(new ResultViewModel<Video>("Conteúdo não encontrado"));
-  }
-
-
-
-  [Route("/api/v1/videos")]
-  [HttpPost]
-  public async Task<IActionResult> PostVideoAsync(
-    [FromBody] EditorVideoViewModel video)
-  {
-
-    if (!ModelState.IsValid)
-      return BadRequest(new ResultViewModel<Video>(ModelState.GetErrors()));
-    try
+    public VideoController(AluraflixContext context)
     {
-      var newVideo = new Video
-      {
-        Title = video.Title,
-        Description = video.Description,
-        Url = video.Url,
-        CategoryId = video.CategoryId == 0 ? 1 : video.CategoryId 
-      };
-      await _context.Videos.AddAsync(newVideo);
-      await _context.SaveChangesAsync();
-      return Created($"api/v1/videos/{newVideo.Id}", new ResultViewModel<Video>(newVideo));
+        _context = context;
     }
-    catch (DbUpdateException)
-    {
-      return BadRequest(new ResultViewModel<Video>("Não foi possível incluir o vídeo.Verifique as informações e tente novamente."));
-    }
-    catch
-    {
-      return StatusCode(500, new ResultViewModel<Video>("Falha interna no servidor"));
-    }
-  }
 
-  [Route("/api/v1/videos/{id}")]
-  [HttpPut]
-  public async Task<IActionResult> UpdateVideoAsync(
-    [FromRoute] int id,
-    [FromBody] EditorVideoViewModel video)
-  {
-    try
+    [Route("/api/v1/videos")]
+    [HttpGet]
+    public async Task<IActionResult> GetAsync(
+       [FromQuery] int page = 0,
+       [FromQuery] int pageSize = 5
+    )
     {
-      var videoToUpdate = await _context.Videos.FirstOrDefaultAsync(x => x.Id == id);
-      if (videoToUpdate != null)
-      {
-        videoToUpdate.Title = video.Title;
-        videoToUpdate.Description = video.Description;
-        videoToUpdate.Url = video.Url;
-        videoToUpdate.CategoryId = video.CategoryId == 0 ? 1 : video.CategoryId;
-        _context.Videos.Update(videoToUpdate);
-        await _context.SaveChangesAsync();
-        return Ok(new ResultViewModel<Video>(videoToUpdate));
-      }
-      else
-        return NotFound(new ResultViewModel<Video>("Conteúdo não encontrado"));
-    }
-    catch
-    {
-      return StatusCode(500, new ResultViewModel<Video>("Falha interna no servidor"));
-    }
-  }
 
-  [Route("/api/v1/videos/{id}")]
-  [HttpDelete]
-  public async Task<IActionResult> DeleteVideoAsync(
-     [FromRoute] int id)
-  {
-    try
-    {
-      var videoToDelete = await _context.Videos.FirstOrDefaultAsync(x => x.Id == id);
-      if (videoToDelete != null)
-      {
-        _context.Videos.Remove(videoToDelete);
-        await _context.SaveChangesAsync();
-        return Ok(new ResultViewModel<Video>("Vídeo deletado com sucesso."));
-      }
-      else
-        return NotFound(new ResultViewModel<Video>("Conteúdo não encontrado"));
+        var videos = await _context
+            .Videos
+            .AsNoTracking()
+            .Skip(page * pageSize)
+            .Take(pageSize)
+            .OrderBy(x => x.Id)
+            .ToListAsync();
+        return Ok(new ResultViewModel<List<Video>>(videos));
     }
-    catch
-    {
-      return StatusCode(500, new ResultViewModel<Video>("Falha ao tentar deletar o vídeo."));
-    }
-  }
 
-   [Route("/api/v1/videos/category/")]
-  [HttpGet]
-  public async Task<IActionResult> GetVideosBySearchCategoryTitle([FromQuery] string search)
-  {
-    if(String.IsNullOrEmpty(search))
+    [Route("/api/v1/videos/{id}")]
+    [HttpGet]
+    public async Task<IActionResult> GetByIdAsync(
+       [FromRoute] int id)
     {
-      var videos = await _context
-        .Videos
-        .AsNoTracking()
-        .Include(x => x.Category)
-        .Select(x => new ListVideosViewModel
+        var video = await _context.Videos.FirstOrDefaultAsync(x => x.Id == id);
+        if (video != null)
+            return Ok(new ResultViewModel<Video>(video));
+        else
+            return NotFound(new ResultViewModel<Video>("Conteúdo não encontrado"));
+    }
+
+
+
+    [Route("/api/v1/videos")]
+    [HttpPost]
+    public async Task<IActionResult> PostVideoAsync(
+      [FromBody] EditorVideoViewModel video)
+    {
+
+        if (!ModelState.IsValid)
+            return BadRequest(new ResultViewModel<Video>(ModelState.GetErrors()));
+        try
         {
-          Id = x.Id,
-          Title = x.Title,
-          Description = x.Description,
-          Url = x.Url,
-          Category = x.Category.Title
+            var newVideo = new Video
+            {
+                Title = video.Title,
+                Description = video.Description,
+                Url = video.Url,
+                CategoryId = video.CategoryId == 0 ? 1 : video.CategoryId
+            };
+            await _context.Videos.AddAsync(newVideo);
+            await _context.SaveChangesAsync();
+            return Created($"api/v1/videos/{newVideo.Id}", new ResultViewModel<Video>(newVideo));
         }
-        )
-        .ToListAsync();
-      return Ok(new ResultViewModel<dynamic>(new { videos }));
-    }
-    try
-    {
-      var videos = await _context
-        .Videos
-        .AsNoTracking()
-        .Include(x => x.Category)
-        .Where(x => x.Category.Title == search)
-        .Select(x => new ListVideosViewModel
+        catch (DbUpdateException)
         {
-          Id = x.Id,
-          Title = x.Title,
-          Description = x.Description,
-          Url = x.Url,
-          Category = x.Category.Title
+            return BadRequest(new ResultViewModel<Video>("Não foi possível incluir o vídeo.Verifique as informações e tente novamente."));
         }
-        )
-        .ToListAsync();
-      return Ok(new ResultViewModel<dynamic>(new { videos }));
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<Video>("Falha interna no servidor"));
+        }
     }
-    catch
+
+    [Route("/api/v1/videos/{id}")]
+    [HttpPut]
+    public async Task<IActionResult> UpdateVideoAsync(
+      [FromRoute] int id,
+      [FromBody] EditorVideoViewModel video)
     {
-      return StatusCode(500, new ResultViewModel<Category>("Falha interna no servidor"));
+        try
+        {
+            var videoToUpdate = await _context.Videos.FirstOrDefaultAsync(x => x.Id == id);
+            if (videoToUpdate != null)
+            {
+                videoToUpdate.Title = video.Title;
+                videoToUpdate.Description = video.Description;
+                videoToUpdate.Url = video.Url;
+                videoToUpdate.CategoryId = video.CategoryId == 0 ? 1 : video.CategoryId;
+                _context.Videos.Update(videoToUpdate);
+                await _context.SaveChangesAsync();
+                return Ok(new ResultViewModel<Video>(videoToUpdate));
+            }
+            else
+                return NotFound(new ResultViewModel<Video>("Conteúdo não encontrado"));
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<Video>("Falha interna no servidor"));
+        }
     }
-  }
+
+    [Route("/api/v1/videos/{id}")]
+    [HttpDelete]
+    public async Task<IActionResult> DeleteVideoAsync(
+       [FromRoute] int id)
+    {
+        try
+        {
+            var videoToDelete = await _context.Videos.FirstOrDefaultAsync(x => x.Id == id);
+            if (videoToDelete != null)
+            {
+                _context.Videos.Remove(videoToDelete);
+                await _context.SaveChangesAsync();
+                return Ok(new ResultViewModel<Video>("Vídeo deletado com sucesso."));
+            }
+            else
+                return NotFound(new ResultViewModel<Video>("Conteúdo não encontrado"));
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<Video>("Falha ao tentar deletar o vídeo."));
+        }
+    }
+
+    [Route("/api/v1/videos/category/")]
+    [HttpGet]
+    public async Task<IActionResult> GetVideosBySearchCategoryTitle(
+        [FromQuery] string? search,
+        [FromQuery] int page = 0,
+        [FromQuery] int pageSize = 5
+    )
+    {
+        if (string.IsNullOrEmpty(search))
+        {
+            var videos = await _context
+              .Videos
+              .AsNoTracking()
+              .Include(x => x.Category)
+              .Select(x => new ListVideosViewModel
+              {
+                  Id = x.Id,
+                  Title = x.Title,
+                  Description = x.Description,
+                  Url = x.Url,
+                  Category = x.Category.Title
+              }
+              )
+              .Skip(page * pageSize)
+              .Take(pageSize)
+              .OrderBy(x => x.Id)
+              .ToListAsync();
+            return Ok(new ResultViewModel<dynamic>(new { videos }));
+        }
+        try
+        {
+            var videos = await _context
+              .Videos
+              .AsNoTracking()
+              .Include(x => x.Category)
+              .Where(x => x.Category.Title == search)
+              .Select(x => new ListVideosViewModel
+              {
+                  Id = x.Id,
+                  Title = x.Title,
+                  Description = x.Description,
+                  Url = x.Url,
+                  Category = x.Category.Title
+              }
+              )
+              .Skip(page * pageSize)
+              .Take(pageSize)
+              .OrderBy(x => x.Id)
+              .ToListAsync();
+            return Ok(new ResultViewModel<dynamic>(new { videos }));
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<Category>("Falha interna no servidor"));
+        }
+    }
 }
