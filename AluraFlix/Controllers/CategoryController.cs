81d4429ce51951c40b3572da3b32f675c9ce7f83
Carlos Rodrigues <crodrigsantos@gmail.com>
2022-11-15 22:53:42 -0300

    Pagination added

--- AluraFlix/Controllers/CategoryController.cs
+++ AluraFlix/Controllers/CategoryController.cs
@@ -12,143 +12,183 @@ namespace Aluraflix.Controllers;
 [ApiController]
 public class CategoryController : ControllerBase
 {
-  private AluraflixContext _context;
+    private AluraflixContext _context;
 
-  public CategoryController(AluraflixContext context)
-  {
-    _context = context;
-  }
+    public CategoryController(AluraflixContext context)
+    {
+        _context = context;
+    }
 
-  [Route("/api/v1/categories")]
-  [HttpGet]
-  public async Task<IActionResult> GetAsync()
-  {
-    var Categories = await _context.Categories.ToListAsync();
-    return Ok(new ResultViewModel<List<Category>>(Categories));
-  }
+    [Route("/api/v1/categories")]
+    [HttpGet]
+    public async Task<IActionResult> GetAsync(
+        [FromQuery] int page = 0,
+        [FromQuery] int pageSize = 5
+      )
+    {
+        var count = await _context.Categories.AsNoTracking().CountAsync();
+        var categories = await _context
+            .Categories
+            .AsNoTracking()
+            .Skip(page * pageSize)
+            .Take(pageSize)
+            .OrderBy(x => x.Id)
+            .ToListAsync();
+        return Ok(new ResultViewModel<dynamic>
+                (new
+                {
+                    total = count,
+                    page,
+                    pageSize,
+                    categories
+                }
+                )
+            );
+    }
 
-  [Route("/api/v1/categories/{id}")]
-  [HttpGet]
-  public async Task<IActionResult> GetByIdAsync(
+    [Route("/api/v1/categories/{id}")]
+    [HttpGet]
+    public async Task<IActionResult> GetByIdAsync(
     [FromRoute] int id)
-  {
-    var category = await _context.Categories.FirstOrDefaultAsync(x => x.Id == id);
-    if (category != null)
-      return Ok(new ResultViewModel<Category>(category));
-    else
-      return NotFound(new ResultViewModel<Category>("Categoria não encontrada."));
-  }
-
-  [Route("/api/v1/categories")]
-  [HttpPost]
-  public async Task<IActionResult> PostCategoryAsync(
-    [FromBody] EditorCategoryViewModel category)
-  {
-
-    if (!ModelState.IsValid)
-      return BadRequest(new ResultViewModel<Category>(ModelState.GetErrors()));
-    try
     {
-      var newCategory = new Category
-      {
-        Title = category.Title,
-        Color = category.Color,
-        Videos = null
-      };
-      await _context.Categories.AddAsync(newCategory);
-      await _context.SaveChangesAsync();
-      return Created($"/api/categories/{newCategory.Id}", new ResultViewModel<Category>(newCategory));
+        var category = await _context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+        if (category != null)
+            return Ok(new ResultViewModel<Category>(category));
+        else
+            return NotFound(new ResultViewModel<Category>("Categoria não encontrada."));
     }
-    catch (DbUpdateException e)
-    {
-      return BadRequest(new ResultViewModel<Category>("Não foi possível incluir a categoria.Verifique as informações e tente novamente."));
-    }
-    catch
-    {
-      return StatusCode(500, new ResultViewModel<Category>("Falha interna no servidor"));
-    }
-  }
 
-  [Route("/api/v1/categories/{id}")]
-  [HttpPut]
-  public async Task<IActionResult> UpdateCategoryAsync(
-    [FromRoute] int id,
-    [FromBody] EditorCategoryViewModel category)
-  {
-    if (!ModelState.IsValid)
-      return BadRequest(new ResultViewModel<Category>(ModelState.GetErrors()));
-
-    try
+    [Route("/api/v1/categories")]
+    [HttpPost]
+    public async Task<IActionResult> PostCategoryAsync(
+      [FromBody] EditorCategoryViewModel category)
     {
-      var categoryToUpdate = await _context.Categories.FirstOrDefaultAsync(x => x.Id == id);
-      if (categoryToUpdate != null)
-      {
 
-        categoryToUpdate.Title = category.Title;
-        categoryToUpdate.Color = category.Color;
-
-        _context.Categories.Update(categoryToUpdate);
-        await _context.SaveChangesAsync();
-        return Ok(new ResultViewModel<Category>(categoryToUpdate));
-      }
-      else
-        return NotFound(new ResultViewModel<Category>("Categoria não encontrada."));
-    }
-    catch
-    {
-      return StatusCode(500, new ResultViewModel<Category>("Falha interna no servidor"));
-    }
-  }
-
-  [Route("/api/v1/categories/{id}")]
-  [HttpDelete]
-  public async Task<IActionResult> DeleteCategoryAsync(
-    [FromRoute] int id)
-  {
-    try
-    {
-      var categoryToDelete = await _context.Categories.FirstOrDefaultAsync(x => x.Id == id);
-      if (categoryToDelete != null)
-      {
-        _context.Categories.Remove(categoryToDelete);
-        await _context.SaveChangesAsync();
-        return Ok(new ResultViewModel<Category>("Categoria deletada com sucesso."));
-      }
-      else
-        return NotFound(new ResultViewModel<Category>("Categoria não encontrada."));
-    }
-    catch
-    {
-      return StatusCode(500, new ResultViewModel<Category>("Falha ao tentar deletar a categoria."));
-    }
-  }
-
-  [Route("/api/v1/categories/{id}/videos")]
-  [HttpGet]
-  public async Task<IActionResult> GetVideosByCategory([FromRoute] int id)
-  {
-    try
-    {
-      var videos = await _context
-        .Videos
-        .AsNoTracking()
-        .Include(x => x.Category)
-        .Where(x => x.Category.Id == id)
-        .Select(x => new ListVideosViewModel
+        if (!ModelState.IsValid)
+            return BadRequest(new ResultViewModel<Category>(ModelState.GetErrors()));
+        try
         {
-          Id = x.Id,
-          Title = x.Title,
-          Description = x.Description,
-          Url = x.Url,
-          Category = x.Category.Title
+            var newCategory = new Category
+            {
+                Title = category.Title.ToUpper(),
+                Color = category.Color,
+                Videos = null
+            };
+            await _context.Categories.AddAsync(newCategory);
+            await _context.SaveChangesAsync();
+            return Created($"/api/categories/{newCategory.Id}", new ResultViewModel<Category>(newCategory));
         }
-        )
-        .ToListAsync();
-      return Ok(new ResultViewModel<dynamic>(new { videos }));
+        catch (DbUpdateException e)
+        {
+            return BadRequest(new ResultViewModel<Category>("Não foi possível incluir a categoria.Verifique as informações e tente novamente."));
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<Category>("Falha interna no servidor"));
+        }
     }
-    catch
+
+    [Route("/api/v1/categories/{id}")]
+    [HttpPut]
+    public async Task<IActionResult> UpdateCategoryAsync(
+      [FromRoute] int id,
+      [FromBody] EditorCategoryViewModel category)
     {
-      return StatusCode(500, new ResultViewModel<Category>("Falha interna no servidor"));
+        if (!ModelState.IsValid)
+            return BadRequest(new ResultViewModel<Category>(ModelState.GetErrors()));
+
+        try
+        {
+            var categoryToUpdate = await _context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if (categoryToUpdate != null)
+            {
+
+                categoryToUpdate.Title = category.Title.ToUpper();
+                categoryToUpdate.Color = category.Color;
+
+                _context.Categories.Update(categoryToUpdate);
+                await _context.SaveChangesAsync();
+                return Ok(new ResultViewModel<Category>(categoryToUpdate));
+            }
+            else
+                return NotFound(new ResultViewModel<Category>("Categoria não encontrada."));
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<Category>("Falha interna no servidor"));
+        }
     }
-  }
+
+    [Route("/api/v1/categories/{id}")]
+    [HttpDelete]
+    public async Task<IActionResult> DeleteCategoryAsync(
+      [FromRoute] int id)
+    {
+        try
+        {
+            var categoryToDelete = await _context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if (categoryToDelete != null)
+            {
+                _context.Categories.Remove(categoryToDelete);
+                await _context.SaveChangesAsync();
+                return Ok(new ResultViewModel<Category>("Categoria deletada com sucesso."));
+            }
+            else
+                return NotFound(new ResultViewModel<Category>("Categoria não encontrada."));
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<Category>("Falha ao tentar deletar a categoria."));
+        }
+    }
+
+    [Route("/api/v1/categories/{id}/videos")]
+    [HttpGet]
+    public async Task<IActionResult> GetVideosByCategory(
+        [FromRoute] int id,
+        [FromQuery] int page = 0,
+        [FromQuery] int pageSize = 5
+    )
+    {
+        try
+        {
+            var count = await _context
+                .Videos
+                .AsNoTracking()
+                .Where(x => x.Category.Id == id)
+                .CountAsync();
+            var videos = await _context
+              .Videos
+              .AsNoTracking()
+              .Include(x => x.Category)
+              .Where(x => x.Category.Id == id)
+              .Select(x => new ListVideosViewModel
+              {
+                  Id = x.Id,
+                  Title = x.Title,
+                  Description = x.Description,
+                  Url = x.Url,
+                  Category = x.Category.Title
+              }
+              )
+              .Skip(page * pageSize)
+              .Take(pageSize)
+              .OrderBy(x => x.Id)
+              .ToListAsync();
+            return Ok(new ResultViewModel<dynamic>
+               (new
+               {
+                   total = count,
+                   page = page,
+                   pageSize,
+                   videos
+               }
+               )
+           );
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<Category>("Falha interna no servidor"));
+        }
+    }
 }
